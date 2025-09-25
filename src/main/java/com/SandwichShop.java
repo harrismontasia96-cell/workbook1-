a93ee961cebd6f3e66e4f54dc815ad676e991921
montasia harris <mharris@my.yearupunited.com>
2025-09-25 11:33:23 -0400

    exercise-sandwichshop-workbookweek1

---
+++ src/main/java/com/SandwichShop.java
@@ -0,0 +1,25 @@
+package com;
+
+import java.util.Scanner;
+
+public class SandwichShop {
+
+
+  public static void main(String[] args) {
+        String name ="student 17";
+        String b ="senior 65 = 20% discount";
+      Scanner input = new Scanner(System.in);
+
+      System.out.print("Enter your age: ");
+      int age=input.nextInt();
+      input.nextLine();
+
+        if( age > 80) {
+            System.out.println("receives no discount");
+        } else {
+            System.out.println("receives no discount");
+        }
+
+
+    }
+}
